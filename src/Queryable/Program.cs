2da30d19c57ea916c54ad705005321dc7856b448
Morgan Kenyon <morganwkenyon@gmail.com>
2019-10-01 05:39:26 -0500

    Adding Queryprovider through part 1.

---
+++ src/Queryable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Queryable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/Queryable/Query.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Queryable
+{
+    public class Query<T> : IQueryable<T>, IQueryable, IEnumerable<T>, IEnumerable, IOrderedQueryable<T>, IOrderedQueryable
+    {
+        QueryProvider provider;
+
+        Expression expression;
+
+        public Query(QueryProvider provider)
+        {
+            if (provider == null)
+            {
+                throw new ArgumentNullException("provider");
+            }
+
+            this.provider = provider;
+            this.expression = Expression.Constant(this);
+        }
+
+        public Query(QueryProvider provider, Expression expression)
+        {
+            if (provider == null)
+            {
+                throw new ArgumentNullException("provider");
+            }
+
+            if (expression == null)
+            {
+                throw new ArgumentNullException("expression");
+            }
+
+            if (!typeof(IQueryable<T>).IsAssignableFrom(expression.Type))
+            {
+                throw new ArgumentOutOfRangeException("expression");
+            }
+            this.provider = provider;
+            this.expression = expression;
+        }
+
+        Type IQueryable.ElementType => typeof(T);
+
+        Expression IQueryable.Expression => expression;
+        IQueryProvider IQueryable.Provider => provider;
+
+        public IEnumerator<T> GetEnumerator() => ((IEnumerable<T>)provider.Execute(expression)).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)provider.Execute(expression)).GetEnumerator();
+
+        public override string ToString() => provider.GetQueryText(expression);
+    }
+}
